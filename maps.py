4f588f1c2303904425053b63cc2efc957fdb108f
Jason Stephens <jassteph@redhat.com>
2019-08-15 14:17:24 -0400

    Remove env variable from msg in the map

--- maps.py
+++ maps.py
@@ -3,10 +3,10 @@ import config
 
 PUP = {"produce_topic": "platform.upload.advisor",
        "consume_topic": "platform.inventory.host-ingress",
-       "msg": payloads.build_payload(config.COMPONENT)
+       "msg": payloads.build_payload("pup")
        }
 
 HBI = {"produce_topic": "platform.inventory.host-ingress",
        "consume_topic": "platform.inventory.host-egress",
-       "msg": payloads.build_payload(config.COMPONENT)
+       "msg": payloads.build_payload("hbi")
        }
